11150cfc8430b4b54f213ae55df6f4e1a9dce2cb
Jannik Steinbach <jannikac@mailbox.org>
2016-12-19 23:52:18 +0000

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,40 +7,71 @@
 
 ###################################################################################
 
-arg1=$1
 USER=$(whoami)
 
 
 function help {
 	printf "
-	Usage: install.sh [OPTION]
+	Usage: setup.sh [OPTION]
 	
-  	-h, --help		prints this menu
+  	-h, --help		prints this
   	-i, --install		installs files
+	-r, --remove		uninstalls files
 "
 }
 
-function install {
-	
-	# touching temporary file to gain sudo priviliges
+function remove {
+	printf "touch temporary file to gain sudo priviliges.. "
 	sudo touch .tmp
-	
-	#copying systemd/* to /etc/systemd/system/ and enabling seafile.service and seahub.service
-	sudo cp systemd/* /etc/systemd/system/
-	sudo systemctl enable seafile.service
-	sudo systemctl enable seahub.service
-	
-	#removing temporary file
+	printf "done\n"
+
+	printf "disabling seafile.service and seahub.service.. "
+	sudo systemctl disable seafile.service
+	sudo systemctl disable seahub.service
+	printf "done\n"
+
+	printf "removing /etc/systemd/system/seafile.service and /etc/systemd/system/seahub.service.. "
+	sudo rm /etc/systemd/system/seafile.service
+	sudo rm /etc/systemd/system/seahub.service
+	printf "done\n"
+
+	printf "removing temporary file.. "
 	rm .tmp -f
+	printf "done\n"
 
 }
 
-case $arg1 in
+
+function install {
+	
+	printf "touch temporary file to gain sudo priviliges.. "
+	sudo touch .tmp
+	printf "done\n"
+	
+	printf "copying systemd/* to /etc/systemd/system/.. "
+	sudo cp systemd/* /etc/systemd/system/
+	printf "done\n"
+
+	printf "enabling seafile.service and seahub.service.. "
+	sudo systemctl enable seafile.service
+	sudo systemctl enable seahub.service
+	printf "done\n"
+	
+	printf "removing temporary file.. "
+	rm .tmp -f
+	printf "done\n"
+
+}
+
+case $1 in
 	--help|-h )
 		help
 		;;
 	--install|-i) 
 		install
+		;;
+	--remove|-r)
+		remove
 		;;
 	* )
 		help
